d59ec0748e6b082f612b6dcc0f9a9d976ffb4ead
Filip Hruška <filip.hruska@student.gyarab.cz>
2023-01-24 09:59:39 +0100

    fachá oboje

--- python_intro/kurzy.py
+++ python_intro/kurzy.py
@@ -1,6 +1,9 @@
 import urllib.request
+#import httpx
 
-link = urllib.request.urlopen("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt;jsessionid=034B7D5C9C0D6A4CE2ED6786D5B6B5D6?date=17.01.2023")
+link = urllib.request.urlopen("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=23.01.2023")
+#link = httpx.get("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=23.01.2023")
+
 f = str(link.read()).split("\\n")
 for i in range(len(f)):
     if "euro" in f[i]:
