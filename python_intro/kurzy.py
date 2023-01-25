4ba281d383d451cc4f0d2626ed7a15ff273dd9f3
Filip Hruška <filip.hruska@student.gyarab.cz>
2023-01-25 17:36:20 +0100

    asi takhle no

--- python_intro/kurzy.py
+++ python_intro/kurzy.py
@@ -1,8 +1,5 @@
-import urllib.request
 import httpx
-from pprint import pprint
 
-link = urllib.request.urlopen("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=23.01.2023")
 url = ("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=23.01.2023")
 
 a = dict()
@@ -10,19 +7,17 @@ a = dict()
 f = httpx.get(url)
 milan = f.text.split("\n")
 for row in milan[2:-1]:
-    curr = row.split("|")[4]
+    curr = float(row.split("|")[4].replace(",", "."))
+    amount = float(row.split("|")[2])
     name = row.split("|")[1]
-    a[name] = curr
+    a[name] = amount, curr
 
-pprint(a)
-############################################
-"""
-milan = str(input()).split(" ")
-if len(milan) != 2:
-    print("chyba u vstupu")
-else:
-    f = str(link.read()).split("\\n")
-    for i in range(len(f)):
-        if milan[0] in f[i]:
-            print(float(f[i].split("|").pop().replace(",", ".")) * int(milan[1]))
-"""
+czk_user_input = float(input("zadej částku v czk: ").replace(",", "."))
+print("možnosti výběru: ")
+s = ""
+for i in a.items():
+    s += i[0] + ", "
+print(f"možnosti výběru: {s}")
+
+curr_input = input("zadej část, na kterou chceš konvertovat: ")
+print(f"{czk_user_input} czk převedeno na danou měnu: {czk_user_input *  a[curr_input][1] / a[curr_input][0]}")
