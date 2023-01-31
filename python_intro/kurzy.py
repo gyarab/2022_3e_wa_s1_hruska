9dd2548c4cd07c746e89ebd6d31ccfe8e8a7d178
Filip Hruška <filip.hruska@student.gyarab.cz>
2023-01-31 10:01:16 +0100

    kdo má rád matiku ujíždí na pyku

--- python_intro/kurzy.py
+++ python_intro/kurzy.py
@@ -12,11 +12,12 @@ for row in milan[2:-1]:
     name = row.split("|")[1]
     a[name] = amount, curr
 
-czk_user_input = float(input("zadej částku v czk: ").replace(",", "."))
+czk_user_input = float(input("zadej částku: ").replace(",", ".")) #částka v czk
 s = ""
 for i in a.items():
     s += i[0] + ", "
-print(f"možnosti výběru: {s}")
+print(f"možnosti výběru: {s}") #vypsání měn, na které umožňuje program převádět
 
-curr_input = input("zadej část, na kterou chceš konvertovat: ")
-print(f"{czk_user_input} czk převedeno na danou měnu: {czk_user_input *  a[curr_input][1] / a[curr_input][0]}")
+curr_input = input("zadej měnu, na kterou chcete konvertovat: ") #měna na konvertování
+print(f"{czk_user_input} {curr_input} převedeno na czk: {czk_user_input * a[curr_input][1] / a[curr_input][0]}") 
+print(f"{czk_user_input} czk převedeno na {curr_input}: {czk_user_input / a[curr_input][1] * a[curr_input][0]}") 
